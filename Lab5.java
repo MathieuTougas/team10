174336bd2030d53189fab556f378d320cfb47214
MShangriLa <tougas.mathieu@gmail.com>
2017-02-23 13:01:44 -0500

    * Initial Comit with Copy of Lab 5

---
+++ Lab5.java
@@ -0,0 +1,74 @@
+package ev3BallLauncher;
+
+import lejos.hardware.Button;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+
+public class Lab5 {
+	
+	// Static Resources:
+	// Left motor connected to output A
+	// Right motor connected to output D
+	private static final EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("B"));
+	private static final EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("C"));
+	private static final EV3LargeRegulatedMotor catapultMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
+	private static final EV3LargeRegulatedMotor stabilizerMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
+
+	// Constants
+	public static final double WHEEL_RADIUS = 2.1;
+	public static final double TRACK = 15.7;
+	private static final double [][] TARGETS = {{-30.0, 90.0},{0, 90.0}, {30.0, 90.0}};
+
+	public static void main(String[] args) {
+		int buttonChoice;
+
+		// some objects that need to be instantiated
+		
+		final TextLCD t = LocalEV3.get().getTextLCD();
+		final Odometer odometer = new Odometer(leftMotor, rightMotor);
+		final Catapult catapult = new Catapult(leftMotor, rightMotor, catapultMotor, stabilizerMotor, odometer, WHEEL_RADIUS, WHEEL_RADIUS, TRACK);
+		OdometryDisplay odometryDisplay = new OdometryDisplay(odometer,t);
+		
+		do {
+			// clear the display
+			t.clear();
+
+			// ask the user whether the motors should drive in a square or float
+			t.drawString("< Left | Right >", 0, 0);
+			t.drawString("       |        ", 0, 1);
+			t.drawString(" Do    | Fire  ", 0, 2);
+			t.drawString(" Not   |       ", 0, 3);
+			t.drawString(" Press |       ", 0, 4);
+
+			buttonChoice = Button.waitForAnyPress();
+		} while (buttonChoice != Button.ID_LEFT
+				&& buttonChoice != Button.ID_RIGHT);
+
+		if (buttonChoice == Button.ID_LEFT) {
+
+			
+			odometer.start();
+			odometryDisplay.start();
+			
+		} else {
+			// start the odometer, the odometry display and (possibly) the
+			// odometry correction
+			
+			odometer.start();
+			odometryDisplay.start();
+
+			// spawn a new Thread to avoid SquareDriver.drive() from blocking
+			(new Thread() {
+				public void run() {
+					// Fire the catapult
+					catapult.fire(TARGETS);
+				}
+			}).start();
+		}
+		
+		while (Button.waitForAnyPress() != Button.ID_ESCAPE);
+		catapult.disengageStabilizers();
+		System.exit(0);
+	}
+}
